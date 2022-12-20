f7a4d5db8804e57c2a4cbec796afcad450e19e52
destantrii <ervianadestantri563@gmail.com>
2022-12-20 21:03:08 +0700

    Upload Pertama

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+function getYear() {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    document.querySelector("#displayYear").innerHTML = currentYear;
+}
+
+getYear();
+
+function openNav() {
+    document.getElementById("myNav").classList.toggle("menu_width");
+    document
+        .querySelector(".custom_menu-btn")
+        .classList.toggle("menu_btn-style");
+}
